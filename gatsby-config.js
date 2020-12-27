a80358c1122dd0a027ea40b6f0f5e5155519cfc7
Nam Hyuck James, Kim <skagur0329@gmail.com>
2020-12-27 16:07:42 +0900

    added posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "pages",
-        path: "${__dirname}/src/pages",
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "posts",
-        path: "${__dirname}/src/posts",
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: "${__dirname}/src/images",
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-plugin-mdx",
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [".md", ".mdx"],
+        extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
             },
@@ -38,10 +38,10 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-google-fonts",
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: ["roboto mono", "muli:400, 400i, 700, 700i"],
-        display: "swap",
+        fonts: [`roboto mono`, `muli:400, 400i, 700, 700i`],
+        display: `swap`,
       },
     },
   ],
